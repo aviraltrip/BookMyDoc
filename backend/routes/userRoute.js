3f690116a4df2e233ab8eadb7591bfe2c050c798
aviraltrip <aviraltripathi25@gmail.com>
2026-07-08 21:07:59 +0530

    feat: add user profile management with get and update functionality, including image upload

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { registerUser, loginUser } from '../controllers/userController.js'
+import { registerUser, loginUser, getProfile, updateProfile } from '../controllers/userController.js'
+import authUser from '../middlewares/authUser.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 
-
+userRouter.get('/get-profile',getProfile)
 
 export default userRouter
