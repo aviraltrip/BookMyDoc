fd35ea1574016ab23bbcf51f706ca149ef4f8966
aviraltrip <aviraltripathi25@gmail.com>
2026-07-09 08:22:27 +0530

    feat: implement appointment booking functionality with availability checks and data handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary' 
+import doctorModel from '../models/doctorModel.js'
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -120,6 +121,54 @@ const updateProfile = async (req,res) => {
   } catch(error) {
     console.log(error)
     res.json({success:false,message:error.message})
+  }
+}
+
+//API to book appointment
+const bookAppointment = async(req,res) => {
+  try {
+    const {userId, docId, slotDate, slotTime} = req.body
+    const docData = await doctorModel.findById(docId).select('-password')
+
+    if (!docData.available) {
+      return res.json({success:false,message:'Doctor not available'})
+    }
+    
+    let slots_booked = docData.slots_booked
+
+    //checking for slot availability
+    if (slots_booked[slotData]) {
+      if (slots_booked[slotData].includes(slotTime)) {
+        return res.json({success:false,message:'Slot not available'})
+      } else {
+        slots_booked[slotDate].push(slotTime)
+      }
+    } else {
+      slots_booked[slotDate] = []
+      slots_booked[slotDate].push(slotTime)
+    }
+
+    const userData = await userModel.findById(userId).select('-password')
+    delete docData.slots_booked
+
+    const appointmentData = {
+      userId,
+      docId,
+      userData,
+      docData,
+      amount:docData.fees,
+      slotTime,
+      slotDate,
+      date: Date.now()
+    }
+
+    const newAppointment = new appointmentModel(appointmentData)
+    
+    
+  } catch (error) {
+    console.log(error)
+    res.json({success: false, message: error.message})
+    
   }
 }
 export { registerUser, loginUser, getProfile, updateProfile }
